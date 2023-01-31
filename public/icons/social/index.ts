939a23c51633baa929bcc978f7e7c681d2b03db3
vito mohagheghian <vito.mohagheghian@gmail.com>
2023-01-31 17:46:34 +0330

    add about page

--- public/icons/social/index.ts
+++ public/icons/social/index.ts
@@ -2,3 +2,7 @@ export { default as TwitterIcon } from "./twitter.svg";
 export { default as LinkedInIcon } from "./linkedin.svg";
 export { default as FacebookIcon } from "./facebook.svg";
 export { default as InstagramIcon } from "./instagram.svg";
+export { default as DribbleIcon } from "./dribble.svg";
+export { default as TelegramIcon } from "./telegram.svg";
+export { default as WhatsappIcon } from "./whatsapp.svg";
+export { default as InstagramOutlineIcon } from "./instagram-outline.svg";
