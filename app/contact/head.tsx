ad4c0ae0999e0cf47a56e109e649e50acfb7295c
vito mohagheghian <vito.mohagheghian@gmail.com>
2023-01-31 19:28:30 +0330

    add contact page

---
+++ app/contact/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Devs Event • Contact</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/logo.svg" />
+    </>
+  );
+}
